c12c0018d4e53e38fb654cd13665a5605202fc30
Mauricio-Munoz <mauricio.m@moabits.com>
2025-06-04 21:09:14 -0500

    comentarios

--- src/main/java/com/ps/java_avanzado/controller/AutorController.java
+++ src/main/java/com/ps/java_avanzado/controller/AutorController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// localhost:8080/autor/all
 @RestController
 @RequestMapping("/autor")
 @RequiredArgsConstructor
@@ -15,29 +15,33 @@ public class AutorController {
 
 
     private final AutorService autorService;
-
+    //GET localhost:8080/autor/all
     @GetMapping("/all")
     public List<Autor> getAutores(){
         List<Autor> autores =autorService.findAll();
         return autores;
     }
-
+    //GET localhost:8080/autor/id
     @GetMapping("/{id}")
     public Autor getAutoresById(@PathVariable Long id) {
         Autor autores =autorService.findById(id);
         return autores;
     }
-
+    //POST localhost:8080/autor
+    // ->  body del mensaje deben poner un json con la informacion que quiere guardar
+    // QUE INFORMACION La del autor SIN El ID
     @PostMapping
     public Autor saveAutores(@RequestBody Autor autor) {
         return autorService.save(autor);
     }
-
+    //PUT localhost:8080/autor
+    // ->  body del mensaje deben poner un json con la informacion que quiere guardar
+    // QUE INFORMACION La del autor CON El ID que se quiere modificar
     @PutMapping
     public Autor updateAutores(@RequestBody Autor autor){
         return autorService.update(autor);
     }
-
+    //DELETE localhost:8080/autor/id
     @DeleteMapping("/{id}")
     public void deleteAutor(@PathVariable Long id) {
          autorService.delete(id);
